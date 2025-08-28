150d9df3b5e96a1189182d302449ba32465d1d32
Skhsouravhalder <skhsouravhalder@gmail.com>
2025-08-28 20:20:09 +0600

    add p

--- index.js
+++ index.js
@@ -12,22 +12,44 @@ for (let i = 0; i < sd.length; i++) {
     likeNumberSpanTag.innerText = likeNumber;
   });
 }
+// 2
 
-// document.getElementById("h").addEventListener("click", function () {
-//   //   const hHard = document.getElementById("head-heart");
-//   //   const h = hHard.innerText;
-//   //   const num = parseInt(h);
-//   //   num++;
-//   //   h = num;
+const copyButtons = document.querySelectorAll(".cp");
 
-//   const likeNumberSpanTag = document.getElementById("head-heart");
-//   const likeNumberStr = likeNumberSpanTag.innerText;
+function handleCopyClick(event) {
+  const clickedButton = event.currentTarget;
 
-//   let likeNumber = parseInt(likeNumberStr);
+  const card = clickedButton.closest(".card");
 
-//   // Update / Create
-//   likeNumber++;
+  const cpyElement = card.querySelector(".cpy");
 
-//   // Set in UI
-//   likeNumberSpanTag.innerText = likeNumber;
-// });
+  const textToCopy = cpyElement.innerText;
+  //
+  const likeNumberSpanTag = document.getElementById("cop");
+  const likeNumberStr = likeNumberSpanTag.innerText;
+  let likeNumber = parseInt(likeNumberStr);
+  likeNumber++;
+  likeNumberSpanTag.innerText = likeNumber;
+
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => {
+      alert("Your calling number " + textToCopy + " is copied");
+    })
+    .catch((err) => {
+      console.error("Copy failed: ", err);
+    });
+}
+
+copyButtons.forEach((btn) => {
+  btn.addEventListener("click", handleCopyClick);
+});
+
+// 3
+document.getElementById("call").addEventListener("click", function () {
+  const c = document.getElementById("coin");
+  const cN = parseInt(c.innerText);
+  if (cN < 20) {
+    console.log("❌ Not available coins, must 20 coins to call");
+  }
+});
