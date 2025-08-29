778418cb0937637c54a753f2e098dcd8da626ede
Skhsouravhalder <skhsouravhalder@gmail.com>
2025-08-29 19:23:59 +0600

    rs up

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        "custom-1500": "1500px",
+        "custom-1600": "1600px",
+      },
+    },
+  },
+};
